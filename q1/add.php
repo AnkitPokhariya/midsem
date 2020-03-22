c05b35867736c5a41dfcaaa05e4d770f537efe47
AnkitPokhariya <60503071+AnkitPokhariya@users.noreply.github.com>
2020-03-22 14:23:02 +0530

    Add files via upload

---
+++ q1/add.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    <title>Input Form</title>
+    </head>
+    
+    <body>
+    <form action="addrecord.php" method="post">
+      Enrollment No.: <input type="text" name="enrollment" placeholder="Enrollment no."><br>
+      Name: <input type="text" name="name"><br>
+      Age:<input type="text" name="age" placeholder="Age"><br>
+      Course:<input type="text" name="course" placeholder="Course"><br>
+      Branch: <input type="text" name="branch" placeholder="Branch"><br>
+      Contact: <input type="text" name="contact" placeholder="Contact"><br>
+      Email: <input type="text" name="email" placeholder="xyz@abc.com"><br>
+      <input type="submit" name='btn'>
+    </form>
+    </body>
+</html>
